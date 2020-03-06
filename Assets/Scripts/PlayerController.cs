9d7cd1541dce8c5da5568d1128930430c31fe1f9
Ely Gandara <ely.gandara.c@gmail.com>
2020-03-06 09:00:30 -0700

    Basic projects files added, Unity chan added, Basic player controller added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CharacterController characterController;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zMovement = Input.GetAxis("Vertical");
+        float xMovement = Input.GetAxis("Horizontal");
+
+        if (zMovement >= 0 || xMovement <= 0){
+            characterController.SimpleMove(new Vector3(xMovement, 0f, zMovement));
+        }
+
+    }
+}
